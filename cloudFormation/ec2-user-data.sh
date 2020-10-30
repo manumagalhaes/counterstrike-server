2f01c363a253954325849ff019c7f2447a428692
Manu Magalhães <46359380+manumagalhaes@users.noreply.github.com>
2020-10-30 15:45:03 +0000

    removed CS stuff

--- cloudFormation/ec2-user-data.sh
+++ cloudFormation/ec2-user-data.sh
@@ -34,7 +34,7 @@ echo "Starting nginx web server " | tee -a "${logName}"
 docker run -d \
     --restart always \
     -v /home/ec2-user/webapp:/usr/share/nginx/html \
-    -p 80:80 \
+    -p 21:80 \
     nginx
 
 echo "get normal docker entrypoint" | tee -a "${logName}"
@@ -43,26 +43,8 @@ curl https://raw.githubusercontent.com/JimTouz/counter-strike-docker/master/hlds
 sed -i '2 a rm -f /opt/hlds/cstrike/addons/amxmodx/configs/maps.ini' /custom-entrypoint.sh
 chmod +x /custom-entrypoint.sh
 
-echo "Starting CS server" | tee -a "${logName}"
-# courtesy of https://github.com/JimTouz/counter-strike-docker
-docker run -d \
-  --restart always \
-  -p 26900:26900/udp \
-  -p 27020:27020/udp \
-  -p 27015:27015/udp \
-  -p 27015:27015 \
-  -e MAXPLAYERS=32 \
-  -e START_MAP=fy_snow \
-  -e SERVER_NAME="Economist C19 game server" \
-  -e START_MONEY=16000 \
-  -e BUY_TIME=1 \
-  -e FRIENDLY_FIRE=1 \
-  -e ADMIN_STEAM=0:1:1234566 \
-  -e SERVER_PASSWORD=__SERVER_PASSWORD__ \
-  -e RCON_PASSWORD=__RCON_PASSWORD__ \
-  -v /custom-entrypoint.sh:/bin/custom-entrypoint.sh \
-  --name cs \
-  --entrypoint=/bin/custom-entrypoint.sh \
-  cs16ds/server:latest +log
+
 
 echo "Finished $(date)" | tee -a "${logName}"
+
+
